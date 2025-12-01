739e40ddbe4778a86a9b2962d0ee79811f5e7615
hasannasrul <hasanali242424@gmail.com>
2025-12-01 14:13:15 +0530

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="extract_load_s3",
-    version="1.0.0",
+    version="1.0.2",
     description="Unified data ingestion framework to move datasets from SFTP/Oracle/Postgres/Snowflake to S3.",
     author="Hasan",
     author_email="hasanali242424@gmail.com",
